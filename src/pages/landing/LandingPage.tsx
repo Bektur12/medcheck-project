948946e04933e70d739b55c8761ebf6ef0fdc680
Bektur12 <kanybekovbektur22@gmail.com>
2023-04-12 02:15:55 +0600

    started landing-page

---
+++ src/pages/landing/LandingPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { styled } from '@mui/material'
+
+export const LandingPage = () => {
+  return <Container>fdsa</Container>
+}
+
+const Container = styled('div')`
+  padding: 0px 120px 0px 120px;
+  height: 100%;
+  background-color: red;
+`
