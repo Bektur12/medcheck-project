137ec752bf917ee38739dbf3f706a1211cb2979c
Bektur12 <kanybekovbektur22@gmail.com>
2023-04-12 00:08:32 +0600

    add button

---
+++ src/components/UI/Button.tsx
@@ -0,0 +1,34 @@
+import React, { FC } from 'react'
+import { Button as MuiButton, styled, ButtonProps } from '@mui/material'
+
+type BtnProps = ButtonProps & {
+  isDesabled?: boolean
+  width?: string
+}
+
+export const Button: FC<BtnProps> = ({
+  onClick,
+  children,
+  isDesabled = false,
+  width,
+  variant = 'contained',
+}) => {
+  return (
+    <StyledButton width={width} onClick={onClick} disabled={isDesabled} variant={variant}>
+      {children}
+    </StyledButton>
+  )
+}
+
+const StyledButton = styled(MuiButton)(
+  ({ width, variant }: { width?: string; variant: string }) => ({
+    '&.MuiButton-root': {
+      color: variant === 'outlined' ? '#FFFFFF' : '#048741',
+      background: variant === 'outlined' ? '#08DF7D' : '#FFFFFF',
+      width,
+      borderRadius: '24px;',
+      textTransform: 'uppercase',
+      border: variant === 'outlined' ? '1px solid #FFFFFF ' : '1px solid #048741',
+    },
+  }),
+)
