aebfecc3fd386868a72fe08c6caa0c5ca46a9f31
Bektur12 <kanybekovbektur22@gmail.com>
2023-04-12 18:08:17 +0600

    set up the main router

---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { UserRoutes } from '../routes/UserRoutes'
+import AdminRoutes from './AdminRoutes'
+
+const ROLE_CONTROLLER: {
+  [key: string]: JSX.Element
+} = {
+  ['ADMIN']: <AdminRoutes />,
+  ['USER']: <UserRoutes />,
+}
+export const AppRoutes = () => {
+  const ROLE = 'USER'
+  return ROLE_CONTROLLER[ROLE]
+}
---
+++ src/routes/UserRoutes.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const UserRoutes = () => {
+  return <div>UserRoutes</div>
+}
